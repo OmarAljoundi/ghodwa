7e2a74ddfbe1b1b7749786ccf6f0fb74a3566a64
OmarAljoundi <omaraljoundi@gmail.com>
2025-02-16 02:21:33 +0300

    Update README and configure authentication client for production

--- auth.ts
+++ auth.ts
@@ -1,19 +1,22 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { db } from "./db.server";
-import { passkey } from "better-auth/plugins/passkey"
+import { passkey } from "better-auth/plugins/passkey";
 
 export const auth = betterAuth({
-    database: prismaAdapter(db, {
-        provider: "postgresql",
+  database: prismaAdapter(db, {
+    provider: "postgresql",
+  }),
+  appName: "ghodwa-dsahboard",
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
+  plugins: [
+    passkey({
+      rpID: "localhost",
+      rpName: "ghodwa dashboard",
+      origin: process.env.NEXT_PUBLIC_APP_URL,
     }),
-    appName:"ghodwa-dsahboard",
-    emailAndPassword:{
-        
-        enabled:true,
-        autoSignIn:true,
-
-    },
-    plugins:[passkey({rpID:"localhost",rpName:"ghodwa dashboard",origin:"http://localhost:3000"})]
-
+  ],
 });
